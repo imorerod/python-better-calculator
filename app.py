4dd204fd993359e2352c3dd875e96a36954ef2a3
imorerod <idm9191@gmail.com>
2019-08-11 21:15:42 -0500

    built out if statements to accomodate operators

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 
 num1 = float(input('Enter first number: '))
 op = input('Enter an operator: ')
-num2 = float(input('Enter second number'))
+num2 = float(input('Enter second number: '))
 
-
+if op == '+':
+    print(num1 + num2)
+elif op == '-':
+    print(num1 - num2)
+elif op == '/':
+    print(num1 / num2)
+elif op == '*':
+    print(num1 * num2)
+else:
+    print('Invalid operator; choose +, -, /, or *')
 
